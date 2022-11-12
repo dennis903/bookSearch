9cd090e91e56985a9956b24fcf5a6a5db0c6c5f1
이형진 <hyeongjin3@hanmail.net>
2022-11-12 21:07:57 +0900

    fix: 로그인 버튼 카카오 로그인 버튼으로 수정

--- src/components/KakaoLogin.js
+++ src/components/KakaoLogin.js
@@ -2,7 +2,11 @@ import React from "react";
 import { KAKAO_AUTH_URL } from "./Oauth";
 
 function KakaoLogin() {
-  return <a href={KAKAO_AUTH_URL}>카카오 로그인</a>;
+  return (
+    <a href={KAKAO_AUTH_URL}>
+      <img src={"img/kakao_login.png"} alt="*" />
+    </a>
+  );
 }
 
 export default KakaoLogin;
